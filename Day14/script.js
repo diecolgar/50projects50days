b2e20742c7a6d957fa5382cd03e90ed1f9e5129d
Diego Colino García <dcolino@indra.es>
2021-11-15 11:22:11 +0100

    15 November 2021

---
+++ Day14/script.js
@@ -0,0 +1,8 @@
+const closeicon = document.querySelector(".closeicon");
+// const navigation = document.querySelectorAll(".navigation");
+
+closeicon.addEventListener('click', () => {
+    
+    console.log('Hola');
+    
+})
